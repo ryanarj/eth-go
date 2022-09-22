7ace9e271abd40c250a3ba9e2af063f2bff2eac9
ryanarj <ryanarj07@gmail.com>
2022-09-22 15:23:07 -0400

    eth decryption

---
+++ genKeystore/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+func main() {
+	// key := keystore.NewKeyStore("./wallet", keystore.StandardScryptN, keystore.StandardScryptP)
+	passord := "password"
+	// account, err := key.NewAccount(passord)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// log.Println(account.Address)
+	b, err := ioutil.ReadFile("./wallet/UTC--2022-09-22T19-07-47.626847000Z--e09a37bbc5527af01f10947f8bdf5d9065dd244c")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	key, err := keystore.DecryptKey(b, passord)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pData := crypto.FromECDSA(key.PrivateKey)
+	fmt.Println("Private ", hexutil.Encode(pData))
+
+	pubData := crypto.FromECDSAPub(&key.PrivateKey.PublicKey)
+	fmt.Println("Public ", hexutil.Encode(pubData))
+
+	fmt.Println("Add ", crypto.PubkeyToAddress(key.PrivateKey.PublicKey).Hex())
+
+}
